09bc2f4cbb19d8fbc951afc10db2bca66cfc08f4
4l3j0Ok <alejofsarmiento@gmail.com>
2024-01-07 15:29:26 -0300

    Google Analytics scripts

--- src/my_links/modules/config.py
+++ src/my_links/modules/config.py
@@ -13,6 +13,17 @@ class StyleSheet(Enum):
     google_fonts = "https://fonts.googleapis.com/css?family={font_name}&display=swap"
 
 
+class GoogleAnalytics(Enum):
+    TAG = os.getenv("GOOGLE_ANALYTICS_TAG", "G-XXXXXXX")
+    INIT_SCRIPT_URL = f"https://www.googletagmanager.com/gtag/js?id={TAG}"
+    SEND_DATA_SCRIPT = f"""
+window.dataLayer = window.dataLayer || [];
+function gtag(){{window.dataLayer.push(arguments);}}
+gtag('js', new Date());
+gtag('config', '{TAG}');
+"""
+
+
 class Highlight(Enum):
     links = {
         "alejoide": {
@@ -51,37 +62,37 @@ class Social(Enum):
     links = {
         "instagram": {
             "title": "Instagram",
-            "description": "Sígueme en Instagram.",
+            "description": "Visita mi Instagram personal.",
             "icon": "ri:instagram-fill",
             "href": "https://instagram.com/4l3j0"
         },
         "twitter": {
             "title": "Twitter",
-            "description": "Conéctate conmigo en Twitter.",
+            "description": "Mi perfil de Twitter personal.",
             "icon": "fa-brands:twitter",
             "href": "https://twitter.com/alejoide_"
         },
         "youtube": {
             "title": "Youtube | General",
-            "description": "Suscríbete a mi canal de Youtube donde subo edits y contenido variado.",
+            "description": "Mi canal de Youtube donde subo edits y contenido variado.",
             "icon": "fa-brands:youtube",
             "href": "https://www.youtube.com/@alejo_okay"
         },
         "youtube_gaming": {
             "title": "Youtube | Juegos",
-            "description": "Suscríbete a mi canal de Youtube de juegos.",
+            "description": "Mi canal de Youtube de juegos.",
             "icon": "fa-brands:youtube",
             "href": "http://www.youtube.com/@AlejoideFG"
         },
         "twitch": {
             "title": "Twitch",
-            "description": "A veces hago direcots en Twitch.",
+            "description": "A veces hago directos en Twitch.",
             "icon": "fa-brands:twitch",
             "href": "https://twitch.tv/alejoiiide"
         },
         "spotify": {
             "title": "Spotify",
-            "description": "Mis playlists en Spotify.",
+            "description": "Explora mis playlists en Spotify.",
             "icon": "fa-brands:spotify",
             "href": "https://open.spotify.com/user/212fbxafmve6gvfzkq4hdyelq?si=60ac2d5ee1e74fb7"
         }
--- src/my_links/my_links.py
+++ src/my_links/my_links.py
@@ -3,10 +3,10 @@ from .views.header import header
 from .views.links import links
 from .views.footer import footer
 from .styles import styles
+from .modules.config import GoogleAnalytics
 
 
-
-@rx.page(route="/", title="Mis links", description="")
+@rx.page(route="/", title="Alejoide | Mis Links", description="Todos mis links en un solo lugar.")
 def index() -> rx.Component:
     return rx.box(
         header(),
@@ -15,5 +15,12 @@ def index() -> rx.Component:
     )
 
 
-app = rx.App(style=styles.BASE)
+app = rx.App(
+    style=styles.BASE,
+    stylesheets=styles.STYLESHEETS,
+    head_components=[
+        rx.script(src=GoogleAnalytics.INIT_SCRIPT_URL.value),
+        rx.script(GoogleAnalytics.SEND_DATA_SCRIPT.value),
+    ]
+)
 app.compile()
