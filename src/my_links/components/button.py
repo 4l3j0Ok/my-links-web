8761f796a69f890acebe71932991fda36af06e1f
4l3j0Ok <alejofsarmiento@gmail.com>
2024-01-07 20:53:37 -0300

    Removed button inside link (using class_name instead)

--- src/my_links/components/button.py
+++ src/my_links/components/button.py
@@ -5,25 +5,24 @@ from ..styles.sizes import Size
 
 def button(title: str = "", description: str = "", icon: str = "", href: str = "#", **args) -> rx.Component:
     return rx.link(
-        rx.button(
-            rx.hstack(
-                iconify(
-                    icon,
-                    font_size=Size.xlarge.value,
-                    padding_x=Size.xsmall.value
-                ),
-                rx.vstack(
-                    rx.span(title),
-                    rx.span(
-                        description,
-                        font_size=Size.small.value,
-                    ),
-                    align_items="flex-start",
-                ),
+        rx.hstack(
+            iconify(
+                icon,
+                font_size=Size.xlarge.value,
+                padding_x=Size.xsmall.value,
             ),
-            **args
+            rx.vstack(
+                rx.text(title, as_="b"),
+                rx.text(
+                    description,
+                    font_size=Size.small.value,
+                ),
+                align_items="flex-start",
+            ),
         ),
+        class_name="button",
         href=href,
         is_external=True,
         width="100%",
+        **args
     )
--- src/my_links/styles/styles.py
+++ src/my_links/styles/styles.py
@@ -22,27 +22,28 @@ BASE = {
         "background": Palette.pink.value,
         "color": Palette.white.value,
     },
-    rx.Heading: {
-        "font-family": Font.DEFAULT.value,
-        "font-size": Size.xlarge.value,
-        "margin-y": Size.small.value,
-        "width": "100%",
-    },
-    rx.Button: {
+    ".button": {
         "display": "inline-block",
         "overflow": "hidden",
         "white-space": "wrap",
         "text-align": "start",
         "width": "100%",
         "height": "auto",
+        "font-size": Size.medium.value,
         "margin-y": Size.small.value,
-        "padding": Size.medium.value,
+        "padding": Size.normal.value,
         "background": f"{Palette.light_gray.value}",
         ":hover": {
             "background": f"{Palette.xlight_gray.value}"
         },
         "transition": ".3s ease-in-out",
         "border-radius": Size.medium.value,
+    },
+    rx.Heading: {
+        "font-family": Font.DEFAULT.value,
+        "font-size": Size.xlarge.value,
+        "margin-y": Size.small.value,
+        "width": "100%",
     },
     rx.Image: {
         "pointer-events": "none",
@@ -61,7 +62,7 @@ FRAME = {
     "max-width": "600px",
     "align-items": "center",
     "background": Palette.dark_gray.value,
-    "border-radius": Size.medium.value,
+    "border-radius": Size.normal.value,
     "flex-direction": "column",
     "padding": Size.xxlarge.value,
     "margin-top": Size.xlarge.value,
