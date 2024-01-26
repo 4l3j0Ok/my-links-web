e69c77135858d4d5c6367871fe9b6bd3a30d81f2
4l3j0Ok <alejofsarmiento@gmail.com>
2024-01-25 21:21:39 -0300

    Replaced dashes with underscores

--- src/my_links/styles/styles.py
+++ src/my_links/styles/styles.py
@@ -12,46 +12,46 @@ STYLESHEETS = [
 BASE = {
     "background": Palette.black.value,
     "color": f"{Palette.white.value}",
-    "font-family": Font.DEFAULT.value,
-    "font-size": Size.small.value,
+    "font_family": Font.DEFAULT.value,
+    "font_size": Size.small.value,
     "::selection": {
         "background": Palette.pink.value,
         "color": Palette.white.value,
     },
-    "::moz-selection": {
+    "::moz_selection": {
         "background": Palette.pink.value,
         "color": Palette.white.value,
     },
     ".button": {
-        "display": "inline-block",
+        "display": "inline_block",
         "overflow": "hidden",
-        "white-space": "wrap",
-        "text-align": "start",
+        "white_space": "wrap",
+        "text_align": "start",
         "width": "100%",
         "height": "auto",
-        "font-size": Size.medium.value,
-        "margin-y": Size.small.value,
+        "font_size": Size.medium.value,
+        "margin_y": Size.small.value,
         "padding": Size.normal.value,
         "background": f"{Palette.light_gray.value}",
         ":hover": {
             "background": f"{Palette.xlight_gray.value}"
         },
-        "transition": ".3s ease-in-out",
-        "border-radius": Size.medium.value,
+        "transition": ".3s ease_in_out",
+        "border_radius": Size.medium.value,
     },
     rx.Heading: {
-        "font-family": Font.DEFAULT.value,
-        "font-size": Size.xlarge.value,
-        "margin-y": Size.small.value,
+        "font_family": Font.DEFAULT.value,
+        "font_size": Size.xlarge.value,
+        "margin_y": Size.small.value,
         "width": "100%",
     },
     rx.Image: {
-        "pointer-events": "none",
-        "user-select": "none",
+        "pointer_events": "none",
+        "user_select": "none",
     },
     rx.Link: {
         ":hover": {
-            "text-decoration": "none",
+            "text_decoration": "none",
         }
     },
 }
@@ -59,11 +59,11 @@ BASE = {
 
 FRAME = {
     "width": "100%",
-    "max-width": "600px",
-    "align-items": "center",
+    "max_width": "600px",
+    "align_items": "center",
     "background": Palette.dark_gray.value,
-    "border-radius": Size.normal.value,
-    "flex-direction": "column",
+    "border_radius": Size.normal.value,
+    "flex_direction": "column",
     "padding": Size.xxlarge.value,
-    "margin-top": Size.xlarge.value,
+    "margin_top": Size.xlarge.value,
 }
